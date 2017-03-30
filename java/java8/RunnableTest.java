62878fa2f8480fde9936e3f0817572b23f221641
Ryan Liao <ryan_liao@intuit.com>
2017-03-29 18:52:26 -0700

    adding haskell

--- java/java8/RunnableTest.java
+++ java/java8/RunnableTest.java
@@ -1,4 +1,10 @@
 // Runnable Lambda
+
+// Lambda expressions follows this format: (args) -> return
+// (int x, int y) -> x + y
+// () -> 42
+// (String s) -> { System.out.println(s); }
+
 public class RunnableTest {
   public static void main(String[] args) {
     System.out.println("=== RunnableTest ===");
@@ -12,6 +18,7 @@ public class RunnableTest {
     };
 
     // Lambda Runnnable
+    // like case above, no parameters is passed and returned
     Runnable r2 = () -> System.out.println("Hello world two!");
 
     // Testing
