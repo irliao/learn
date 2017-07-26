7a01a5c0b943e0a20c24526c37b2f61a6ba9ad18
Ryan Liao <ianryanliao@gmail.com>
2017-07-25 21:42:09 -0700

    working on haskell week 3

--- haskell/cis194-sp13/week-03/Golf.hs
+++ haskell/cis194-sp13/week-03/Golf.hs
@@ -1,11 +1,16 @@
 module Golf where
 
 {- Exercise 1 -}
--- skips "ABCD"
--- skips "hello!"
--- skips [1]
+-- skips "ABCD" == ["ABCD", "BD", "C", "D"] -- "ABCD" is of type [Char]
+-- skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
+-- skips [1] ==  [[1]]
 -- skips [True,False] == [[True,False], [False]]
+-- skips [True,False,True] == [[True,False], [False]]
+-- skips [a,b,c,d] == [[a,b,c,d], [b,d], [d]]
 -- skips []           == []
- skips :: [a] -> [[a]]
- skips 
+skips :: [a] -> [[a]]
+skips [] = []
+skips (x:[]) = [[x]]
+skips (x:(y:zs)) = [ (x:y:zs), zip (x:y:zs) [1..] ]
+
  
