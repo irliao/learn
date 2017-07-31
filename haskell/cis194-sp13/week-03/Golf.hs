6b2697ef28f75d1e5a3cccad0da702b93f13ff9c
Ryan Liao <ianryanliao@gmail.com>
2017-07-30 18:38:29 -0700

    got haskell week-03 exercise 1 working

--- haskell/cis194-sp13/week-03/Golf.hs
+++ haskell/cis194-sp13/week-03/Golf.hs
@@ -8,17 +8,22 @@ module Golf where
 -- skips [True,False,True] == [[True,False], [False]]
 -- skips [a,b,c,d] == [[a,b,c,d], [b,d], [d]]
 -- skips []           == []
+-- TODO: ask brian and nikhil how to shorten this
 skips :: [a] -> [[a]]
 skips [] = []
--- skips (x:[]) = [[x]]
-skips xs = map f (zip [1..(length xs)] (replicate (length xs) xs))
-              -- where f y = [fst y]
-              where f (y,z) = z
+skips xs = map f (zip [1..(length xs)] (replicate (length xs) xs)) where f (y,z) = foldl (\acc x -> if mod (fst x) y == 0 then acc ++ [(snd x)] else acc) [] (zip [1..(length z)] z)
 
+-- localMaxima :: [Integer] -> [Integer]
+-- localMaxima [] = []
+-- localMaxima xs = 
 
--- map f xs where f (a,b) = 
-
+-- Notes
 -- foldr
 -- let f acc b = (acc + b) in foldl f 0 [1,2,3,4,5] 
 -- let f = (+) in foldl f 0 [1,2,3,4,5]
 -- foldl (+) 0 [1,2,3,4,5]
+--
+-- foldl (\acc x -> acc ++ [x]) [] [1,2,3,4,5] == [1,2,3,4,5]
+-- foldl (\acc x -> if x > 2 then acc ++ [x] else acc) [] [1,2,3,4,5] == [3,4,5]
+-- foldl (\acc x -> if mod (fst x) 2 == 0 then acc ++ (snd x) else acc) "" [(1,"A"), (2,"B"), (3,"C"), (4,"D")] == "BD"
+
--- haskell/cis194-sp13/week-03/test.hs
+++ haskell/cis194-sp13/week-03/test.hs
@@ -1,9 +1,12 @@
-doStuff1 :: [Int] -> Int
-doStuff1 []  = 0
-doStuff1 [_] = 0
-doStuff1 xs  = head xs + (head (tail xs)) 
+-- doStuff1 :: [Int] -> Int
+-- doStuff1 []  = 0
+-- doStuff1 [_] = 0
+-- doStuff1 xs  = head xs + (head (tail xs)) 
 
-doStuff2 :: [Int] -> Int
-doStuff2 []        = 0
-doStuff2 [_]       = 0
-doStuff2 (x1:x2:_) = x1 + x2
+-- doStuff2 :: [Int] -> Int
+-- doStuff2 []        = 0
+-- doStuff2 [_]       = 0
+-- doStuff2 (x1:x2:_) = x1 + x2
+
+foldl f [] [1,2,3,4,5] 
+  where f a b = a ++ b
