b85aead78368e10fa821af8cae19e82007bb7e61
Ryan Liao <ryan_liao@intuit.com>
2017-09-06 19:40:34 -0700

    added haskell-programming chapter3 files

---
+++ haskell/haskell-programming/chapter-03/print3.hs
@@ -0,0 +1,11 @@
+-- print3.hs
+module Print3 where
+
+myGreeting :: String
+-- The above line reads as: "myGreeting has the type String"
+myGreeting = "hello" ++ " world!"
+-- Could also be: "hello" ++ " " ++ "world!"
+-- to obtain the same result.
+
+hello : String
+hello = "hello"
