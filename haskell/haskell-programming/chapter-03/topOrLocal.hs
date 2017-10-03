aec912b99da39697a0b5ae293d498dad69d804b1
Ryan Liao <ianryanliao@gmail.com>
2017-10-02 20:11:48 -0700

    added topOrLocal.hs for haskell programming chapter 03

--- haskell/haskell-programming/chapter-03/topOrLocal.hs
+++ haskell/haskell-programming/chapter-03/topOrLocal.hs
@@ -1,1 +1,12 @@
 module TopOrLocal where
+
+topLevelFunction :: Integer -> Integer
+topLevelFunction x = x + woot + topLevelValue
+  -- where and let introduces local bindings or declarations
+  -- woot has it's type explicitly declared with :: syntax, which isn't necessary as Haskell's type inference would've figured it out
+  where woot :: Integer -- woot is invisible oytside of topLevelFunction
+        woot = 10
+
+topLevelValue :: Integer
+topLevelValue = 5
+
